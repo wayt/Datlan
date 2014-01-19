d56d2c596b320e3da0b64651659156e270239a54
Vagaweb <fabien.casters@epitech.eu>
2014-01-19 22:07:48 +0100

    Formulaire d'inscription

--- www/controllers/PageController.php
+++ www/controllers/PageController.php
@@ -7,8 +7,13 @@ use Application\Form\ContactForm;
 
 class PageController extends AbstractController {
 
+    private $_request;
+    private $_response;
+
     public function init() {
 
+        $this->_request = $this->getApplication()->getRequest();
+        $this->_response = $this->getApplication()->getResponse();
     }
 
     public function indexAction() {
@@ -24,6 +29,22 @@ class PageController extends AbstractController {
     public function contactAction() {
 
         $form = new ContactForm();
+
+        if ($this->_request->isPost())
+        {
+            if ($form->isValid($this->_request->getPost())) {
+
+                $headers = 'From: no-reply@datlan.eu' . "\r\n" .
+                           'Reply-To: no-reply@datlan.eu' . "\r\n" .
+                           'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
+                           'X-Mailer: PHP/' . phpversion();
+
+                mail($user->getEmail(), 'Datlan - Activation de votre compte',
+                    "<p><a href='http://www.datlan.eu/activate-account?key=" . $user->getKey() . "'>Lien d'activation de votre compte</a></p>",
+                    $headers
+                );
+            }
+        }
 
         return $this->render([
             'form' => $form
