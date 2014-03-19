d73ff6b7767cf8b9d23bb7e6f9d799a294e3d61b
Vagaweb <fabien.casters@epitech.eu>
2014-03-19 19:47:34 +0100

    prout

---
+++ www/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_USER',               'lan');
+define('DB_PSWD',               'poneyrause');
+define('DB_NAME',               'datlan');
+define('DB_HOST',               'localhost');
+define('CAPTCHA_PUBLIC_KEY',    '6Lc0Se0SAAAAAJt2NZOp7kbFe2XTQibfvKayNUbL');
+define('CAPTCHA_PRIVATE_KEY',   '6Lc0Se0SAAAAAMDv-3GLKG2V1Ax75w52PCb_Dj96');
+define('EMAIL_BLACKLIST',       'hotmail.fr hotmail.com live.com');
